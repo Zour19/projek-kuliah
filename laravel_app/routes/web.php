cc6350cf2d280b55fdd865d87923985f7fff5952
Zour19 <zourandiha@gmail.com>
2026-06-25 17:47:22 +0700

    merapihkan path

--- laravel_app/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\ShopController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [ShopController::class, 'home'])->name('home');
-Route::get('/category/{slug}', [ShopController::class, 'category'])->name('category.show');
